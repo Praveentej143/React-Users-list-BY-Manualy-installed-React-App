31e4f6a365d64e91d17c902b6fac531600b2b0ce
Your Name <you@example.com>
2022-11-02 14:06:07 +0000

    users list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import UserProfile from "./components/userProfile/index";
+import "./App.css";
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: "https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
+    name: "rahul",
+    role: "Web Developer",
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: "https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
+    name: "rahul",
+    role: "Web Developer",
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: "https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
+    name: "rahul",
+    role: "Web Developer",
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: "https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
+    name: "rahul",
+    role: "Web Developer",
+  },
+];
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
+    <ul>
+      {userDetailsList.map((eachItem) => (
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+);
 
 export default App;
